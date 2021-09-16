e2c553c041625f2023dbe95335e266bdb6c6557d
Andrei Khodko <andrei.khodko@gmail.com>
2021-09-17 01:53:33 +0300

    Split big function

--- hw-1/main.go
+++ hw-1/main.go
@@ -48,6 +48,36 @@ func setSize(size int) Mod {
 	}
 }
 
+func drawMiddle(symbol Symbol, width int, halfHeight int) {
+	for i := 1; i < halfHeight; i++ {
+		for j := i; j > 0; j-- {
+			fmt.Print(" ")
+		}
+		if width != -1 {
+			printSymbol(symbol)
+		}
+		for j := 0; j < width; j++ {
+			fmt.Print(" ")
+		}
+		printSymbol(symbol)
+		width -= 2
+		fmt.Println()
+	}
+	width += 4
+	for i := 2; i < halfHeight; i++ {
+		for j := i; j < halfHeight; j++ {
+			fmt.Print(" ")
+		}
+		printSymbol(symbol)
+		for j := 0; j < width; j++ {
+			fmt.Print(" ")
+		}
+		printSymbol(symbol)
+		width += 2
+		fmt.Println()
+	}
+}
+
 func sandglass(mods ...Mod) {
 	symbol := make(Symbol, settingsSize)
 	symbol[charSetting] = "X"
@@ -63,42 +93,13 @@ func sandglass(mods ...Mod) {
 		return
 	}
 
-	w := width - 4
 	halfHeight := int(float32(width)/2 + 0.5)
-
 	printLine(width, symbol)
-	for i := 1; i < halfHeight; i++ {
-		for j := i; j > 0; j-- {
-			fmt.Print(" ")
-		}
-		if w != -1 {
-			printSymbol(symbol)
-		}
-		for j := 0; j < w; j++ {
-			fmt.Print(" ")
-		}
-		printSymbol(symbol)
-		w -= 2
-		fmt.Println()
-	}
-	w += 4
-	for i := 2; i < halfHeight; i++ {
-		for j := i; j < halfHeight; j++ {
-			fmt.Print(" ")
-		}
-		printSymbol(symbol)
-		for j := 0; j < w; j++ {
-			fmt.Print(" ")
-		}
-		printSymbol(symbol)
-		w += 2
-		fmt.Println()
-	}
+	drawMiddle(symbol, width-4, halfHeight)
 	printLine(width, symbol)
 }
 
 func main() {
-	sandglass()
 	sandglass(setSize(7))
 	sandglass(setChar("O"), setSize(9))
 	sandglass(setSize(10), setColor(31), setChar("S"))
